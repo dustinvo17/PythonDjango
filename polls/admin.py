ad149d76a23c937bae663f43597c42c4a3df263f
Dustin Vo <datvo.vtd@gmail.com>
2020-01-12 00:09:34 -0500

    python django learning

---
+++ polls/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Question,Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['question_text']}),
+        ('Date Information', {'fields':['pub_date']})
+    ]
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice)
